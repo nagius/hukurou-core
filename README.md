30d64a1e1f3ff53f4d02e4631b731c826a99a753
Nicolas Agius <nicolas.agius@lps-it.fr>
2016-03-01 22:14:40 +0000

    Added License

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-New monitoring tool
+Hukurou Core
+============
+
+Hukurou, Another monitoring tool, the modern way.
+
+This software is the core component (API and Database) of the Hukurou monitoning system.
+
 
 THIS IS A DRAFT - NOT PRODUCTION READY
--- lib/hukurou/core/assets.rb
+++ lib/hukurou/core/assets.rb
@@ -1,3 +1,24 @@
+# Hukurou - Another monitoring tool, the modern way.
+# Copyleft 2015 - Nicolas AGIUS <nicolas.agius@lps-it.fr>
+#
+################################################################################
+#
+# This file is part of Hukurou.
+#
+# Hukurou is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
 
 # TODO: make default_device for each DC ??
 # TODO: check default DC ?
@@ -322,4 +343,5 @@ end
 
 class Hukurou::Core::Assets::SubstitutionError < StandardError
 end
+
 # vim: ts=4:sw=4:ai:noet
